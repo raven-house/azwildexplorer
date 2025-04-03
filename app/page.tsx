14d1b03e78cbbd8d710ec23d519f261f526b89fb
satyambnsal <satyamsgsits1994@gmail.com>
2025-04-03 17:01:59 +0530

    feature: Add table of transaction

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,60 @@
+import { Button } from '@/components/ui/button'
 import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import {
+  TableHeader,
+  TableRow,
+  TableHead,
+  TableBody,
+  TableCell,
+  Table,
+} from '@/components/ui/table'
+import { shortenTxnHash } from '@/lib/utils'
 import { ArrowRight, Bell, Box, Code, Layout, MessageSquare } from 'lucide-react'
+
+const TRANSACTION_MOCK_DATA = [
+  {
+    txnHash: '0x0838f46715219fc857bd5da73170ab1e9e869850a0f7578e7565c6eb8017973d',
+    txnStatus: 'Paid',
+    txnType: 'INVOKE_FUNCTION',
+    paymentMethod: 'Credit Card',
+  },
+  {
+    txnHash: '0x0838f46715219fc857bd5da731708jy88s869850a0f7578e7565c6eb801797cd',
+    txnStatus: 'Pending',
+    txnType: 'INVOKE_FUNCTION',
+    paymentMethod: 'PayPal',
+  },
+  {
+    txnHash: '0x0838f46715219fc857bd5da731708jh7js869850a0f7578e7565c6eb8017979kj',
+    txnStatus: 'Unpaid',
+    txnType: 'INVOKE_FUNCTION',
+    paymentMethod: 'Bank Transfer',
+  },
+  {
+    txnHash: '0x0838f46715219fc857bd5da731708jhuujj69850a0f7578e7565c6eb801797ik',
+    txnStatus: 'Paid',
+    txnType: 'INVOKE_FUNCTION',
+    paymentMethod: 'Credit Card',
+  },
+  {
+    txnHash: '0x0838f46715219fc857bd5da73170ssss8s869850a0f7578e7565c6eb801794df',
+    txnStatus: 'Paid',
+    txnType: 'INVOKE_FUNCTION',
+    paymentMethod: 'PayPal',
+  },
+  {
+    txnHash: '0x0838f46715219fc857bd5da731708jsdjs869850a0f7578e7565c6eb8017972s',
+    txnStatus: 'Pending',
+    txnType: 'INVOKE_FUNCTION',
+    paymentMethod: 'Bank Transfer',
+  },
+  {
+    txnHash: '0x0838f46715219fc857bd5da731708jh7js8698508hfr6h78e765c6eb801797xs',
+    txnStatus: 'Unpaid',
+    txnType: 'INVOKE_FUNCTION',
+    paymentMethod: 'Credit Card',
+  },
+]
 
 const DASHBOARD_DATA = [
   {
@@ -78,22 +133,52 @@ const DASHBOARD_DATA = [
 
 export default function Home() {
   return (
-    <main className="pt-20">
-      <div className="grid grid-cols-3 gap-3">
-        {DASHBOARD_DATA.map((data) => {
-          return (
-            <Card key={data.id}>
-              <div className="flex items-center px-6">
-                <div>{data.icon}</div>
-                <CardHeader>
-                  <CardTitle>{data.heading}</CardTitle>
-                  <CardDescription className="text-2xl">{data.value}</CardDescription>
-                </CardHeader>
-              </div>
-            </Card>
-          )
-        })}
-      </div>
+    <main className="pt-20 flex flex-col gap-20">
+      <section>
+        <div className="grid grid-cols-3 gap-3">
+          {DASHBOARD_DATA.map((data) => {
+            return (
+              <Card key={data.id}>
+                <div className="flex items-center px-6">
+                  <div>{data.icon}</div>
+                  <CardHeader>
+                    <CardTitle>{data.heading}</CardTitle>
+                    <CardDescription className="text-2xl">{data.value}</CardDescription>
+                  </CardHeader>
+                </div>
+              </Card>
+            )
+          })}
+        </div>
+      </section>
+
+      <section>
+        <h2 className="text-primary font-bold mb-4">Latest Transactions</h2>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Transaction Hash</TableHead>
+              <TableHead>Type</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Age</TableHead>
+              <TableHead className="text-right">Action</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {TRANSACTION_MOCK_DATA.map((txn) => (
+              <TableRow key={txn.txnHash}>
+                <TableCell className="font-medium">{shortenTxnHash(txn.txnHash)}</TableCell>
+                <TableCell>{txn.txnType}</TableCell>
+                <TableCell>{txn.txnStatus}</TableCell>
+                <TableCell>{txn.paymentMethod}</TableCell>
+                <TableCell className="text-right">
+                  <Button size="sm">Reveal</Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </section>
     </main>
   )
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,13 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function shortenTxnHash(txnHash: string): string {
+  const start = txnHash.substring(0, 6)
+  const end = txnHash.substring(txnHash.length - 4)
+
+  return `${start}…${end}`
 }
